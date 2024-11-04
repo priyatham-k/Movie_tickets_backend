9b75aa92ca1f63f26d54bd5b7a4a35e729b9745c
priyatham-k <priyathamkmikkili@gmail.com>
2024-11-04 16:13:08 -0600

    qwe

--- server.js
+++ server.js
@@ -1,10 +1,29 @@
-const express = require('express');
-const mongoose = require('mongoose');
+// backend/server.js
 
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const authRoutes = require("./routes/auth");
+const theatreRoutes = require("./routes/theatre");
+const cors = require("cors");
 const app = express();
+app.use(
+  cors({
+    origin: "http://localhost:3000", // Allow requests from this origin
+  })
+);
+app.use(bodyParser.json());
+app.use("/api/user", authRoutes);
+app.use("/api/theatre", theatreRoutes);
+// Connect to MongoDB
+mongoose
+  .connect("mongodb://127.0.0.1:27017/Movie_Tickets", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log(err));
 
-app.use(express.json())
-
-app.listen(3000, ()=>{
-    console.log('listening on port 3000');
-})
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
